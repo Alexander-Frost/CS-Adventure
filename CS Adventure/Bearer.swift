bc0e3cb500bde16e4aeef7978cec5d0cc508c5e5
Alex <alexcc17@gmail.com>
2019-10-22 18:01:33 -0400

    Merge Kobe

---
+++ CS Adventure/Bearer.swift
@@ -0,0 +1,9 @@
+//
+//  Bearer.swift
+//  CS Adventure
+//
+//  Created by Alex on 10/22/19.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+import Foundation
---
+++ CS Adventure/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  CS Adventure
+//
+//  Created by Alex on 10/22/19.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+import Foundation
