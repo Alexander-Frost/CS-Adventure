8491493a8bfcbfd2915570d362e16e3cd4f67111
Alex <alexcc17@gmail.com>
2019-10-23 14:26:58 -0400

    Create Direction Enum.

---
+++ CS Adventure/Models/Direction.swift
@@ -0,0 +1,16 @@
+//
+//  Direction.swift
+//  CS Adventure
+//
+//  Created by Alex on 10/23/19.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+import Foundation
+
+enum Direction: String {
+    case north = "n"
+    case south = "s"
+    case east = "e"
+    case west = "w"
+}
